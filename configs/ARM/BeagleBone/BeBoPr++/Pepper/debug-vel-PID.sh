5af8d129dc3f31e1b3b5ed34fb847af68f876e1a
bdebruijn <bdebruijn@luminize.nl>
2014-09-03 23:38:33 +0000

    velocity extruding: change configuration with extrusion offsets

--- configs/ARM/BeagleBone/BeBoPr++/Pepper/debug-vel-PID.sh
+++ configs/ARM/BeagleBone/BeBoPr++/Pepper/debug-vel-PID.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 # filament speed
-halcmd setp lincurve.1.x-val-00 0.0
-halcmd setp lincurve.1.x-val-01 0.31
-halcmd setp lincurve.1.x-val-02 0.62
-halcmd setp lincurve.1.x-val-03 0.93
-halcmd setp lincurve.1.x-val-04 1.21
-halcmd setp lincurve.1.x-val-05 1.55
+halcmd setp lincurve.0.x-val-00 0.0
+halcmd setp lincurve.0.x-val-01 0.0005
+halcmd setp lincurve.0.x-val-02 1
+halcmd setp lincurve.0.x-val-03 2
+halcmd setp lincurve.0.x-val-04 3
+halcmd setp lincurve.0.x-val-05 4
 
-halcmd setp lincurve.1.y-val-00 0.0
-halcmd setp lincurve.1.y-val-01 0.005
-halcmd setp lincurve.1.y-val-02 0.015
-halcmd setp lincurve.1.y-val-03 0.04
-halcmd setp lincurve.1.y-val-04 0.09
-halcmd setp lincurve.1.y-val-05 0.13
+#halcmd setp lincurve.1.y-val-00 0
+#halcmd setp lincurve.1.y-val-01 0
+#halcmd setp lincurve.1.y-val-02 0.1
+#halcmd setp lincurve.1.y-val-03 0.1
+#halcmd setp lincurve.1.y-val-04 0.15
+#halcmd setp lincurve.1.y-val-05 0.2
+
+#halcmd setp lincurve.0.y-val-00 0
+#halcmd setp lincurve.0.y-val-01 0
+#halcmd setp lincurve.0.y-val-02 0
+#halcmd setp lincurve.0.y-val-03 0
+#halcmd setp lincurve.0.y-val-04 0
+#halcmd setp lincurve.0.y-val-05 0
+
+halcmd setp lincurve.0.y-val-00 0.00
+halcmd setp lincurve.0.y-val-01 0.01
+halcmd setp lincurve.0.y-val-02 0.07
+halcmd setp lincurve.0.y-val-03 0.18
+halcmd setp lincurve.0.y-val-04 0.24
+halcmd setp lincurve.0.y-val-05 0.3
 
 #halcmd setp hal_pru_generic.stepgen.00.maxaccel 3200
 #halcmd setp hal_pru_generic.stepgen.01.maxaccel 3200
