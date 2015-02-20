f56f5e6c48e5c6c360a01371574592a46154f4d2
bdebruijn <bdebruijn@luminize.nl>
2015-02-20 13:53:07 +0000

    config: save changes before rebase delayline-cleaned

--- configs/ARM/BeagleBone/BeBoPr++/Pepper/debug-vel-PID.sh
+++ configs/ARM/BeagleBone/BeBoPr++/Pepper/debug-vel-PID.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # filament speed
-halcmd setp lincurve.0.x-val-00 0.0
-halcmd setp lincurve.0.x-val-01 0.0005
-halcmd setp lincurve.0.x-val-02 1
-halcmd setp lincurve.0.x-val-03 2
-halcmd setp lincurve.0.x-val-04 3
-halcmd setp lincurve.0.x-val-05 4
+#halcmd setp lincurve.0.x-val-00 0.0
+#halcmd setp lincurve.0.x-val-01 0.0005
+#halcmd setp lincurve.0.x-val-02 2
+#halcmd setp lincurve.0.x-val-03 4
+#halcmd setp lincurve.0.x-val-04 6
+#halcmd setp lincurve.0.x-val-05 8
 
 #halcmd setp lincurve.1.y-val-00 0
 #halcmd setp lincurve.1.y-val-01 0
@@ -14,19 +14,27 @@ halcmd setp lincurve.0.x-val-05 4
 #halcmd setp lincurve.1.y-val-04 0.15
 #halcmd setp lincurve.1.y-val-05 0.2
 
-#halcmd setp lincurve.0.y-val-00 0
-#halcmd setp lincurve.0.y-val-01 0
-#halcmd setp lincurve.0.y-val-02 0
-#halcmd setp lincurve.0.y-val-03 0
-#halcmd setp lincurve.0.y-val-04 0
-#halcmd setp lincurve.0.y-val-05 0
+#lichtblauwe stretchlet 1
+halcmd setp lincurve.0.x-val-00 0.0
+halcmd setp lincurve.0.x-val-01 0.15
+halcmd setp lincurve.0.x-val-02 0.75
+halcmd setp lincurve.0.x-val-03 1.5
+halcmd setp lincurve.0.x-val-04 60
+halcmd setp lincurve.0.x-val-05 80
 
-halcmd setp lincurve.0.y-val-00 0.00
-halcmd setp lincurve.0.y-val-01 0.01
-halcmd setp lincurve.0.y-val-02 0.07
-halcmd setp lincurve.0.y-val-03 0.18
-halcmd setp lincurve.0.y-val-04 0.24
-halcmd setp lincurve.0.y-val-05 0.3
+halcmd setp lincurve.0.y-val-00 0
+halcmd setp lincurve.0.y-val-01 0.0
+halcmd setp lincurve.0.y-val-02 0.04
+halcmd setp lincurve.0.y-val-03 0.12
+halcmd setp lincurve.0.y-val-04 0.12
+halcmd setp lincurve.0.y-val-05 0.12
+
+#halcmd setp lincurve.0.y-val-00 0.00
+#halcmd setp lincurve.0.y-val-01 0.01
+#halcmd setp lincurve.0.y-val-02 0.07
+#halcmd setp lincurve.0.y-val-03 0.18
+#halcmd setp lincurve.0.y-val-04 0.24
+#halcmd setp lincurve.0.y-val-05 0.3
 
 #halcmd setp hal_pru_generic.stepgen.00.maxaccel 3200
 #halcmd setp hal_pru_generic.stepgen.01.maxaccel 3200
@@ -34,6 +42,6 @@ halcmd setp lincurve.0.y-val-05 0.3
 #halcmd setp hal_pru_generic.stepgen.00.maxvel 200
 #halcmd setp hal_pru_generic.stepgen.01.maxvel 200
 #halcmd setp hal_pru_generic.stepgen.02.maxvel 200
-#halcmd setp hal_pru_generic.stepgen.03.maxaccel 2350
+halcmd setp hal_pru_generic.stepgen.03.maxaccel 5000
 #halcmd setp hal_pru_generic.stepgen.03.position-scale -2050
 #halcmd sets e0.temp.set 215
